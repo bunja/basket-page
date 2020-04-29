9918786f09bf41a558dcdd5b8cbf964f1aa15423
Larisa Kosareva <kos.lv@yandex.com>
2020-04-29 21:30:06 +0200

    buy button

--- my-basket/src/App.js
+++ my-basket/src/App.js
@@ -4,91 +4,93 @@ import './App.css';
 import { useDispatch, useSelector } from "react-redux";
 
 import {
-  receiveBasketInfo,
-  removeItem,
-  upItem,
-  downItem
+    receiveBasketInfo,
+    removeItem,
+    upItem,
+    downItem
 } from "./actions";
 
 function App() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(receiveBasketInfo());
-  }, []);
+    useEffect(() => {
+        dispatch(receiveBasketInfo());
+    }, []);
 
-  const basket = useSelector(state => state && state.basket);
+    const basket = useSelector(state => state && state.basket);
 
-  if (basket == null) {
+    if (basket == null) {
+        return (
+            <div>ЖОПА</div>
+        );
+    }
+    const items = basket.items;
+    const vat = basket.vat;
+    const subtotal = basket.subtotal;
+    const total = basket.total;
+
+    // const removeItem = (itemId) => {
+    //   console.log("itemId", itemId);
+    // };
+
+
     return (
-      <div>ЖОПА</div>
+        <div className="App">
+            <div class="container">
+                <h1>AKQA</h1>
+                <h2>Your Basket</h2>
+                <p>Items you have added to your basket are shown below.</p>
+                <p>Adjust the quantities or remove items before counting your purchase</p>
+
+                <table>
+                    <tr class="header">
+                        <th>Product</th>
+                        <th>Price</th>
+                        <th>Qty</th>
+                        <th>Cost</th>
+                    </tr>
+                    {items.map(item => {
+                        return (
+
+                            <tr key={item.id} className="item-row">
+                                <td>{item.product}</td>
+                                <td>{item.price}</td>
+                                <td>{item.qty}
+                                    <div onClick={() => {
+                                        dispatch(upItem(item.id));
+                                    }}>🔼</div>
+                                    <div onClick={() => {
+                                        dispatch(downItem(item.id));
+                                    }}>🔽</div>
+                                </td>
+                                <td>{item.cost}</td>
+                                <td><span onClick={() => {
+                                    dispatch(removeItem(item.id));
+                                }}>🗑️</span></td>
+                            </tr>
+                        );
+                    })}
+
+                    <tr>
+                        <td colspan="3">Subtotal</td>
+                        <td>{subtotal}</td>
+                    </tr>
+                    <tr>
+                        <td colspan="3">VAT @ 20%</td>
+                        <td>{vat}</td>
+                    </tr>
+                    <tr></tr>
+                    <tr>
+                        <td colspan="3">Total Cost</td>
+                        <td>{total}</td>
+                    </tr>
+                </table>
+                <button onClick={() => {
+                    console.log("basket", basket)
+                }}>Buy Now</button>
+            </div>
+        </div>
     );
-  }
-  const items = basket.items;
-  const vat = basket.vat;
-  const subtotal = basket.subtotal;
-  const total = basket.total;
-
-  // const removeItem = (itemId) => {
-  //   console.log("itemId", itemId);
-  // };
-  
-
-  return (
-    <div className="App">
-      <div class="container">
-        <h1>AKQA</h1>
-        <h2>Your Basket</h2>
-        <p>Items you have added to your basket are shown below.</p>
-        <p>Adjust the quantities or remove items before counting your purchase</p>
-
-        <table>
-            <tr class="header">
-                <th>Product</th>
-                <th>Price</th>
-                <th>Qty</th>
-                <th>Cost</th>
-            </tr>
-            {items.map(item => {
-                    return (
-                        
-                        <tr key={item.id} className="item-row">
-                            <td>{item.product}</td>
-                            <td>{item.price}</td>
-                            <td>{item.qty}
-                                <div onClick={() => {
-                                  dispatch(upItem(item.id));
-                                }}>🔼</div>
-                                <div onClick={() => {
-                                  dispatch(downItem(item.id));
-                                }}>🔽</div>
-                            </td>
-                            <td>{item.cost}</td>
-                            <td><span onClick={() => {
-                              dispatch(removeItem(item.id));
-                            }}>🗑️</span></td>
-                        </tr>
-                    );
-                })}
-            
-            <tr>
-                <td colspan="3">Subtotal</td>
-                <td>{subtotal}</td>
-            </tr>
-            <tr>
-                <td colspan="3">VAT @ 20%</td>
-                <td>{vat}</td>
-            </tr>
-            <tr></tr>
-            <tr>
-                <td colspan="3">Total Cost</td>
-                <td>{total}</td>
-            </tr>
-        </table>
-        <button>Buy Now</button>
-    </div>
-    </div>
-  );
 }
 
 export default App;
